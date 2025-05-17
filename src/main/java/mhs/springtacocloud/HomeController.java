1b5a055e2f23208af0bd1dc8016d6b1ad1f1d45c
MHS-20 <muhamad.salman2001@gmail.com>
2025-05-17 10:55:56 +0200

    feat: added home page controller, view and test

---
+++ src/main/java/mhs/springtacocloud/HomeController.java
@@ -0,0 +1,12 @@
+package mhs.springtacocloud;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home"; //logical name of the view
+    }
+}
